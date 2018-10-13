0712b14bdfd3fb7c24373aa0f0e14c5e7a8e8e7d
Jakub Martyčák <martycakjakub@gmail.com>
2018-10-13 21:01:08 +0200

    Add typography components

---
+++ app/components/Typography/Heading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Heading = styled.Text`
+  font-size: 32px;
+  color: #000;
+`;
---
+++ app/components/Typography/Paragraph.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Paragraph = styled.Text`
+  font-size: 16px;
+  margin-bottom: 24px;
+`;
